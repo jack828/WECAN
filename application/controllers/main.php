b34a8c95e40569fdb495c88879d1cc6326e09263
Jack Burgess <jackburgess124@gmail.com>
2017-02-19 22:48:11 +0000

    Lint

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,82 +1,76 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Main extends CI_Controller {
-	 
-	 function __construct()
-	{
-		parent::__construct();	 
+
+	function __construct() {
+		parent::__construct();
 		$this->load->database();
 		$this->load->helper('url');
 		$this->load->library('grocery_CRUD');
 		$this->load->library('table');
 	}
-	
-	public function index()
-	{	
+
+	public function index() {
 		$this->load->view('header');
 		$this->load->view('home');
 	}
-	
-	public function orders()
-	{	
+
+	public function orders() {
 		$this->load->view('header');
 		$crud = new grocery_CRUD();
 		$crud->set_theme('datatables');
-		
+
 		//table name exact from database
 		$crud->set_table('orders');
-		
+
 		//give focus on name used for operations e.g. Add Order, Delete Order
 		$crud->set_subject('Order');
 		$crud->fields('invoiceNo','date', 'custID', 'items');
-		
+
 		//set the foreign keys to appear as drop-down menus
 		// ('this fk column','referencing table', 'column in referencing table')
 		$crud->set_relation('custID','customers','custID');
-		
+
 		//many-to-many relationship with link table see grocery crud website: www.grocerycrud.com/examples/set_a_relation_n_n
 		//('give a new name to related column for list in fields here', 'join table', 'other parent table', 'this fk in join table', 'other fk in join table', 'other parent table's viewable column to see in field')
 		$crud->set_relation_n_n('items', 'order_items', 'items', 'invoice_no', 'item_id', 'itemDesc');
-		
+
 		//form validation (could match database columns set to "not null")
 		$crud->required_fields('invoiceNo', 'date', 'custID');
-		
+
 		//change column heading name for readability ('columm name', 'name to display in frontend column header')
 		$crud->display_as('custID', 'CustomerID');
-		
+
 		$output = $crud->render();
 		$this->orders_output($output);
 	}
-	
-	function orders_output($output = null)
-	{
+
+	function orders_output($output = null) {
 		//this function links up to corresponding page in the views folder to display content for this table
 		$this->load->view('orders_view.php', $output);
 	}
 
-	public function items()
-	{	
+	public function items() {
 		$this->load->view('header');
 		$crud = new grocery_CRUD();
 		$crud->set_theme('datatables');
-		
+
 		$crud->set_table('items');
 		$crud->set_subject('item');
 		$crud->fields('itemID', 'itemDesc', 'orders');
 		$crud->required_fields('itemID', 'itemDesc');
 		$crud->set_relation_n_n('orders', 'order_items', 'orders', 'item_id', 'invoice_no', 'invoiceNo');
 		$crud->display_as('itemDesc', 'Description');
-		
+
 		$output = $crud->render();
 		$this->items_output($output);
 	}
-	
-	function items_output($output = null)
-	{
+
+	function items_output($output = null) {
 		$this->load->view('items_view.php', $output);
 	}
-	public function customers()
-	{	
+
+	public function customers() {
 		$this->load->view('header');
 		$crud = new grocery_CRUD();
 		$crud->set_theme('datatables');
@@ -91,18 +85,16 @@ class Main extends CI_Controller {
 		$crud->display_as('custPostcode', 'Postcode');
 		$crud->display_as('custTel', 'Phone');
 		$crud->display_as('custEmail', 'Email');
-		
+
 		$output = $crud->render();
 		$this->cust_output($output);
 	}
-	
-	function cust_output($output = null)
-	{
+
+	function cust_output($output = null) {
 		$this->load->view('cust_view.php', $output);
 	}
-	
-	public function orderline()
-	{	
+
+	public function orderline() {
 		$this->load->view('header');
 		$crud = new grocery_CRUD();
 		$crud->set_theme('datatables');
@@ -117,36 +109,31 @@ class Main extends CI_Controller {
 		$crud->display_as('item_id', 'ItemID');
 		$crud->display_as('itemQty', 'Quantity');
 		$crud->display_as('itemPrice', 'Price');
-		
+
 		$output = $crud->render();
 		$this->orderline_output($output);
 	}
-	
-	function orderline_output($output = null)
-	{
+
+	function orderline_output($output = null) {
 		$this->load->view('orderline_view.php', $output);
 	}
-	
-	public function querynav()
-	{	
+
+	public function querynav() {
 		$this->load->view('header');
 		$this->load->view('querynav_view');
 	}
-		
-	public function query1()
-	{	
+
+	public function query1() {
 		$this->load->view('header');
 		$this->load->view('query1_view');
 	}
-	
-	public function query2()
-	{	
+
+	public function query2() {
 		$this->load->view('header');
 		$this->load->view('query2_view');
 	}
-	
-	public function blank()
-	{	
+
+	public function blank() {
 		$this->load->view('header');
 		$this->load->view('blank_view');
 	}
