58d6e27c5bdbfd7022052cf0e0a55daba367d099
Jack Burgess <jackburgess124@gmail.com>
2017-02-25 18:30:20 +0000

    Add FontAwesome for icons

--- application/views/header.php
+++ application/views/header.php
@@ -5,6 +5,7 @@
   <title>WECAN</title>
   <link rel="stylesheet" type="text/css" href="/WECAN/assets/grocery_crud/css/custom.css">
   <link rel="stylesheet" type="text/css" href="/WECAN/assets/grocery_crud/css/ui/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="/WECAN/assets/grocery_crud/js/jquery-1.10.2.min.js"></script>
   <script src="/WECAN/assets/grocery_crud/js/bootstrap.min.js"></script>
 </head>
