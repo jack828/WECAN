73872744632256deaf5091c7df9b753de9cccc3e
Jack Burgess <jackburgess124@gmail.com>
2017-02-25 16:58:29 +0000

    Add navigation bar

--- application/views/header.php
+++ application/views/header.php
@@ -12,20 +12,46 @@
   <div>
     <div class="nav-group">
       <h2>WECAN</h2>
-      <div class="user-control">
-        <p><?php echo 'Hello ' . $username ?></p>
-        <a class="nav-btn" href='<?php echo site_url('main/logout')?>'>Logout</a>
-      </div>
     </div>
     <hr class="hr-thick">
-    <ul class="nav">
-      <li><a class="nav-btn" href='<?php echo site_url('main')?>'>Home</a></li>
-      <li><a class="nav-btn" href='<?php echo site_url('main/matches')?>'>Matches</a></li>
-      <li><a class="nav-btn" href='<?php echo site_url('main/teams')?>'>Teams</a></li>
-      <li><a class="nav-btn" href='<?php echo site_url('main/competitors')?>'>Competitors</a></li>
-      <li><a class="nav-btn" href='<?php echo site_url('main/venues')?>'>Venues</a></li>
-      <li><a class="nav-btn" href='<?php echo site_url('main/cards')?>'>Cards</a></li>
-    </ul>
   </div>
+
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?php echo site_url('main')?>">Home</a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="nav navbar-nav">
+          <li class="<?php echo (!preg_match('/matches/', uri_string()) ?: 'active'); ?>">
+            <a href="<?php echo site_url('main/matches')?>">Matches</a>
+          </li>
+          <li class="<?php echo (!preg_match('/teams/', uri_string()) ?: 'active'); ?>">
+            <a href="<?php echo site_url('main/teams')?>">Teams</a>
+          </li>
+          <li class="<?php echo (!preg_match('/competitors/', uri_string()) ?: 'active'); ?>">
+            <a href="<?php echo site_url('main/competitors')?>">Competitors</a>
+          </li>
+          <li class="<?php echo (!preg_match('/venues/', uri_string()) ?: 'active'); ?>">
+            <a href="<?php echo site_url('main/venues')?>">Venues</a>
+          </li>
+          <li class="<?php echo (!preg_match('/cards/', uri_string()) ?: 'active'); ?>">
+            <a href="<?php echo site_url('main/cards')?>">Cards</a>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><p class="navbar-text">Logged in as <?php echo $username; ?></p></li>
+          <li><a href="<?php echo site_url('main/logout')?>">Logout</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </body>
 </html>
