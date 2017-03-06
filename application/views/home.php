803aabed717e1574efdc3e55c679261dfc3805e3
Jack Burgess <jackburgess124@gmail.com>
2017-03-06 22:54:14 +0000

    Make the homepage look 'nice'

--- application/views/home.php
+++ application/views/home.php
@@ -1,16 +1,16 @@
 <div class="right_col" role="main">
   <div class="">
-    <div class="page-title">
-      <div class="title_left">
-        <h1>Welcome to the WECAN system</h1>
+    <div class="x_panel">
+      <div class="x_title">
+        <h1>WECAN System Home</h2>
+        <div class="clearfix"></div>
       </div>
-    </div>
-    <p>This system allows staff at International Widgets to manage stocked items, customers and orders.</p>
-    <p>Click one of the navigation links to begin.</p>
-
-    <div>
-      <img id="cogs" src="/WECAN/assets/images/cogs.jpg" alt="Cogs and gears" height="260" width="380">
-      <!--Image credits: http://www.wallpaperdecor.com.au/murals/scandinavian-wallpaper-decor/typography-collection/cogs-gears-e21324/ -->
+      <div class="x_content">
+        <p>Welcome to the WECAN System.</p>
+        <p>Use the links in the sidebar to nagivate.</p>
+        <img id="cogs" src="/WECAN/assets/images/cogs.jpg" alt="Cogs and gears" height="130" width="190">
+        <!--Image credits: http://www.wallpaperdecor.com.au/murals/scandinavian-wallpaper-decor/typography-collection/cogs-gears-e21324/ -->
+      </div>
     </div>
   </div>
 </div>
