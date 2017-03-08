f4aa0958bb55bef8e5c9068ed21ee6bef56e1118
Jack Burgess <jackburgess124@gmail.com>
2017-03-08 00:51:55 +0000

    General > Sections

--- application/views/header.php
+++ application/views/header.php
@@ -55,7 +55,7 @@
             <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
-                <h3>General</h3>
+                <h3>Sections</h3>
                 <ul class="nav side-menu">
                   <li>
                     <a href="<?php echo site_url('main'); ?>">
